a74469016dbc4659b6bf0d9661dcd08e02cc8505
DamiFur <damian.a.furman@gmail.com>
2023-08-19 19:11:01 -0300

    fix for saving current date

--- scrapper.py
+++ scrapper.py
@@ -1,9 +1,9 @@
-import newspaper
+import newspaper3k
 import mongo
 import time
 from datetime import datetime
 
-diarios = {"LaNacion": "https://www.lanacion.com.ar/", "LID": "https://www.laizquierdadiario.com/", "Clarin": "https://www.clarin.com/", "PO": "https://www.prensaobrera.com", "P12": "https://www.pagina12.com.ar/", "Ambito": "https://www.ambito.com", "Cronista": "https://www.cronista.com", "Cronica": "https://www.cronica.com.ar", "Infobae": "https://www.infobae.com", "Tiempo": "https://www.tiempoar.com.ar"}
+diarios = {"LID": "https://www.laizquierdadiario.com/", "LaNacion": "https://www.lanacion.com.ar/", "Clarin": "https://www.clarin.com/", "PO": "https://www.prensaobrera.com", "P12": "https://www.pagina12.com.ar/", "Ambito": "https://www.ambito.com", "Cronista": "https://www.cronista.com", "Cronica": "https://www.cronica.com.ar", "Infobae": "https://www.infobae.com", "Tiempo": "https://www.tiempoar.com.ar"}
 
 def getAttributes(article):
 	return {
@@ -19,7 +19,7 @@ while True:
 	memoize = False
 	for nombre in diarios:
 		print("Descargando noticias de {}".format(nombre))
-		diario = newspaper.build(diarios[nombre], memoize_articles=memoize)
+		diario = newspaper3k.build(diarios[nombre], memoize_articles=memoize)
 
 		print("Obtuvimos {} articulos nuevos".format(len(diario.articles)))
 
@@ -33,8 +33,8 @@ while True:
 				if not mongo.check_if_exists({"text": article.text}, nombre):
 					print("lala")
 					attributes = getAttributes(article)
-					if attributes.publish_date == None or attributes.publish_date == "":
-						attributes.publish_date = datetime.now()
+					if attributes["publish_date"] == None or attributes["publish_date"] == "":
+						attributes["publish_date"] = datetime.now()
 					print("HERE")
 					mongo.store(attributes, nombre)
 					guardados += 1
